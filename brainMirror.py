cf7e61a59d1a2b7523b9a6d34d737003b1165a9d
InVoker <120738638+Invoker13022006@users.noreply.github.com>
2023-01-19 14:21:00 +0300

    Add files via upload

---
+++ brainMirror.py
@@ -0,0 +1,22 @@
+import requests
+import bs4
+
+try:
+    responseWeather=requests.get('https://yandex.ru/pogoda/')
+    pageTemper=bs4.BeautifulSoup(responseWeather.content)
+    pageWindSpeed = bs4.BeautifulSoup(responseWeather.content)
+
+    responseTime=requests.get('https://time100.ru')
+    pageTime=bs4.BeautifulSoup(responseTime.content)
+
+    temper=pageTemper.find('span', 'temp__value temp__value_with-unit')
+    time=pageTime.find('span', 'time')
+    windspeed=pageWindSpeed.find('span', 'wind-speed')
+
+
+    temper=temper.text
+    time=time.text
+    windspeed=windspeed.text
+except ConnectionError:
+    print('Проверьте ваше подключение к интернету')
+print(windspeed)
