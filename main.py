0b2769c7116b6bf6d964cfbe590b2ab7ef2e29d1
Zdertis420 <118454718+Zdertis420@users.noreply.github.com>
2023-02-28 20:39:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,38 @@
 import sys
+from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
-import brainMirror
-
-temp = brainMirror.temper
-windSpeed = brainMirror.windspeed
-time = brainMirror.time
-
+from brainMirror import temp, conditions, time
 
 class Win(QWidget):
 
     def paintEvent(self, event):
-        font = QFont("Times New Roman", 50)
-        font.setItalic(False)
+
+        font = QFont("Times new Roman", 50)
 
         painter = QPainter(self)
         painter.setFont(font)
 
         painter.save()
-        painter.translate(100, 100)
-        painter.scale(0.7, 0.7)
+        painter.translate(150, 100)
+        painter.scale(2, 2)
         painter.rotate(0)
-        painter.drawText(0, 0, time)
-        painter.restore()
-
-        painter.save()
-        painter.translate(100, 200)
-        painter.scale(0.7, 0.7)
-        painter.rotate(0)
-        painter.drawText(0, 0, temp)
+        painter.drawText(0, 0, str(time))
         painter.restore()
 
         painter.save()
         painter.translate(100, 300)
         painter.scale(0.7, 0.7)
         painter.rotate(0)
-        painter.drawText(0, 0, f'{windSpeed} м/с')
+        painter.drawText(0, 0, str(round(temp[-1], 1)))
         painter.restore()
 
+        painter.save()
+        painter.translate(200, 300)
+        painter.scale(0.9, 0.9)
+        painter.rotate(0)
+        painter.drawText(0, 0, str(conditions[-1]))
+        painter.restore()
 
 app = QApplication(sys.argv)
 win = Win()
